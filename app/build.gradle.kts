634f31b9c3f38cb6b510284a6058d605c5f54c9d
ayoungyi <yibbiayoung@gmail.com>
2026-04-29 02:06:45 +0900

    버전수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,18 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
+    kotlin("android")
 }
 
 android {
     namespace = "kr.co.yellowpass.parent"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "kr.co.yellowpass.parent"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -30,8 +29,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
